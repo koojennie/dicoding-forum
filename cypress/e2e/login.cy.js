66344fd5fa900374f4bc8129dbd80a681443524e
jennie <aliyajenny14@gmail.com>
2026-01-09 21:50:21 +0700

    adding nav check

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -71,6 +71,6 @@ describe('Login spec', () => {
     cy.location('pathname').should('eq', '/');
 
     cy.get('nav').contains('Threads').should('be.visible');
-    cy.get('button').contains('Logout').should('be.visible');
+    cy.get('nav').contains('Logout').should('be.visible');
   });
 });
