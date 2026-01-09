7e73a0b8c63f316039764f6ce0f533773a330d89
jennie <aliyajenny14@gmail.com>
2026-01-09 21:45:11 +0700

    adding button visible

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -71,6 +71,6 @@ describe('Login spec', () => {
     cy.location('pathname').should('eq', '/');
 
     cy.get('nav').contains('Threads').should('be.visible');
-    cy.contains('button', 'Logout').should('be.visible');
+    cy.get('button').contains('Logout').should('be.visible');
   });
 });
