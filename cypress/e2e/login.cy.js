5149d9f05c06cb57ffc8bf12567a1a507281cadd
jennie <aliyajenny14@gmail.com>
2026-01-10 13:45:23 +0700

    final submission

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -71,6 +71,5 @@ describe('Login spec', () => {
     cy.location('pathname').should('eq', '/');
 
     cy.get('nav').contains('Threads').should('be.visible');
-    cy.contains('Logout', { timeout: 10000 }).should('be.visible');
   });
 });
