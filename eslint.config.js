7e7026aa117106dfba3733862c61bd038895199b
jennie <aliyajenny14@gmail.com>
2026-01-11 21:28:26 +0700

    fixing eslint error in config.js files

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import pluginCypress from "eslint-plugin-cypress/flat";
-import { defineConfig } from "eslint/config";
-import daStyle from "eslint-config-dicodingacademy";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import { defineConfig } from 'eslint/config';
+import daStyle from 'eslint-config-dicodingacademy';
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { globals: {...globals.browser, ...globals.node} } },
-    pluginReact.configs.flat.recommended,
-    pluginCypress.configs.recommended,
-    daStyle,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginReact.configs.flat.recommended,
+  pluginCypress.configs.recommended,
+  daStyle,
   {
     rules: {
-      "linebreak-style": "off",
+      'linebreak-style': 'off',
     },
   },
 ]);
